0f7a8193050d7a4b3442973400ec7a2cdcb2c705
linan <linan@linandemacbook-pro.local>
2016-06-14 11:25:39 +0800

    Initial Commit

---
+++ ApplePay/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ApplePay
+//
+//  Created by linan on 16/6/14.
+//  Copyright © 2016年 linan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
